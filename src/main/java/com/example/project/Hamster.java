5b164f79dd31deee574550d94b6b8adab51828f8
MishaneaKrutoi <mishkazelea@mail.ru>
2025-04-24 21:11:52 +0200

    Update Hamster.java

--- src/main/java/com/example/project/Hamster.java
+++ src/main/java/com/example/project/Hamster.java
@@ -2,5 +2,13 @@ package com.example.project;
 
 public class Hamster {
     //TODO: Add a private string field name, a constructor with parameter name and a corresponding getter
-}
+    private String name;
 
+    public Hamster(String name){
+        this.name = name;
+    }
+
+    private String getName(){
+        return this.name;
+    }
+}
